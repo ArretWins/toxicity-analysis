3619ca9ca56325bd9d914bf1b10e3647e28799b8
Artem <prohorenkoartem111@gmail.com>
2025-03-31 23:38:14 +0200

    1

--- toxic_comment_project/settings.py
+++ toxic_comment_project/settings.py
@@ -20,13 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jqp6v-%=1snm5dyvg!liu16_24h^$ym*$+r301ue*@9r#+k5@k'
+SECRET_KEY = os.getenv('SECRET_KEY', 'default-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = []
-
+# Allowed hosts
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
 # Application definition
 
